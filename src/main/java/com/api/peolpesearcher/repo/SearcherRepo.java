8ed229d8f22a1ae892f50ac2a1258a5acb6ac682
Tn-ko <99136238+Tn-ko@users.noreply.github.com>
2023-01-21 11:05:39 +0300

    Update SearcherRepo.java

--- src/main/java/com/api/peolpesearcher/repo/SearcherRepo.java
+++ src/main/java/com/api/peolpesearcher/repo/SearcherRepo.java
@@ -2,16 +2,13 @@ package com.api.peolpesearcher.repo;
 
 import com.api.peolpesearcher.model.Searcher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 
 
 public interface SearcherRepo extends JpaRepository<Searcher, Long> {
     void deleteSearcherById(Long id); //making query by method name
-    Optional<Searcher> findByUsername(String username);
+   
 
     Optional<Searcher> findSearcherById(Long id);
 }
