d3d8b131fea2adef9dbea035e1526a0b44dcb97c
Daniil Tarasov <99136238+dantarasov@users.noreply.github.com>
2024-03-08 16:37:24 +0300

    authorization and roles added

---
+++ src/main/java/com/api/peolpesearcher/payload/request/SignUpRequest.java
@@ -0,0 +1,79 @@
+package com.api.peolpesearcher.payload.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String name;
+
+    @NotBlank
+    @Size(max = 10)
+    private String gender;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String phone;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() { return gender; }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+}
